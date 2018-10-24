dcd78d104e9fb48a86a6e1446c2b2e24f0658682
yaobin.a.liu <yaobiin.a.liu@accenture.com>
2018-10-24 14:19:03 +0800

    SpringBoot config

---
+++ src/main/java/com/better/crudportal/CrudportalApplication.java
@@ -0,0 +1,12 @@
+package com.better.crudportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudportalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudportalApplication.class, args);
+    }
+}
